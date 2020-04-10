7aca87723bffb727d6864c8e926b70eaf8bf5730
Kevin T <kdt0116@tamu.edu>
2020-04-09 23:12:32 -0500

    implemented recursive page table management

--- P4/part3/page_table.C
+++ P4/part3/page_table.C
@@ -25,11 +25,11 @@ PageTable::PageTable()
     head = NULL;
 
     // page directory frame allocation
-    unsigned int pd_frame_num = (unsigned int)kernel_mem_pool->get_frames(1);
+    unsigned long pd_frame_num = kernel_mem_pool->get_frames(1);
     page_directory = (unsigned long *)(pd_frame_num * PAGE_SIZE);
 
     // first page table: do direct mapping for first 4MB
-    unsigned int pt_frame_num = (unsigned int)kernel_mem_pool->get_frames(1);
+    unsigned long pt_frame_num = kernel_mem_pool->get_frames(1);
     unsigned long *page_table = (unsigned long *)(pt_frame_num * PAGE_SIZE);
     for (unsigned int frame_num = 0; frame_num < ENTRIES_PER_PAGE; frame_num++)
     {
@@ -48,6 +48,9 @@ PageTable::PageTable()
         // 2: ...010 |supervisor|r/w|present|
         page_directory[page_table_num] = 2;
     }
+
+    // set last entry to page_directory for recursive paging
+    page_directory[1023] = ((unsigned long)page_directory) | 1;
 
     Console::puts("Constructed Page Table object\n");
 }
@@ -69,13 +72,16 @@ void PageTable::enable_paging()
 
 void PageTable::handle_fault(REGS *_r)
 {
-    unsigned long *page_directory = current_page_table->page_directory;
+    // In recursive paging the page directory address is
+    // |1023|1023|0| = 0xFFFFF000
+    unsigned long *page_directory = (unsigned long *)0xFFFFF000;
     unsigned long access_addr = read_cr2();
     unsigned long p1 = access_addr >> 22;
     unsigned long p2 = (access_addr & 0x003FFFFF) >> 12;
 
     if (!current_page_table->check_address(access_addr))
     {
+        Console::putui(p1);
         Console::puts("check_address failed for address: ");
         Console::putui(access_addr);
         Console::puts(" (this in hexa ");
@@ -89,22 +95,26 @@ void PageTable::handle_fault(REGS *_r)
     if (!(page_directory[p1] & 1))
     {
         // allocate frame for page table
-        unsigned int pt_frame_num = (unsigned int)kernel_mem_pool->get_frames(1);
-        unsigned long *page_table = (unsigned long *)(pt_frame_num * PAGE_SIZE);
+        unsigned long pt_frame_num = kernel_mem_pool->get_frames(1);
+
+        // In recursive page the page table address is
+        // |1023|p1|0| = 0xFFB00000 | (p1 << 12)
+        unsigned long *page_table = (unsigned long *)(0xFFC00000 | (p1 << 12));
+
+        // 3: ...011 |supervisor|r/w|present|
+        page_directory[p1] = (pt_frame_num << 12) | 3;
+
         for (unsigned int frame_num = 0; frame_num < ENTRIES_PER_PAGE; frame_num++)
         {
             // 6: ...110 |supervisor|r/w|present|
             page_table[frame_num] = 6;
         }
-
-        // 3: ...011 |supervisor|r/w|present|
-        page_directory[p1] = (pt_frame_num << 12) | 3;
     }
 
     // allocate single page in page table
-    unsigned int page_table_frame = page_directory[p1] >> 12;
+    unsigned long page_table_frame = page_directory[p1] >> 12;
     unsigned long *page_table = (unsigned long *)(page_table_frame << 12);
-    unsigned int frame_num = (unsigned int)process_mem_pool->get_frames(1);
+    unsigned long frame_num = process_mem_pool->get_frames(1);
 
     page_table[p2] = (frame_num << 12) | 7;
     Console::puts("handled page fault\n");
